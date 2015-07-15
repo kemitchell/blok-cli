22da5ab9217718507edeb96d51aa8ed0c1f7e2aa
Kyle Mitchell <kyle@kemitchell.com>
2015-07-15 01:40:53 -0700

    Pass parse function to blok-get

--- get.js
+++ get.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   var get = require('blok-get')
-    .bind(this, require('./level')())
+    .bind(this, require('./level')(), require('blok-parse'))
   var digest = process.argv[3]
   get(digest, function(error, value) {
     console.log(require('blok-stringify')(value)) }) }
