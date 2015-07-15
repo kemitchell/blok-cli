78d1dc41f57bcd54df8582ec06e0d70ff0da879f
Kyle Mitchell <kyle@kemitchell.com>
2015-07-15 01:50:31 -0700

    Improve get and put subcommands

--- get.js
+++ get.js
@@ -1,6 +1,15 @@
 module.exports = function() {
-  var get = require('blok-get')
-    .bind(this, require('./level')(), require('blok-parse'))
+  function showUsage() {
+    console.error('Usage: blok get <SHA-256 hex digest>')
+    process.exit(1) }
+  if (process.argv.length < 4) {
+    showUsage() }
   var digest = process.argv[3]
-  get(digest, function(error, value) {
-    console.log(require('blok-stringify')(value)) }) }
+  if (!require('is-sha-256-hex-digest')(digest)) {
+    showUsage() }
+  require('blok-get')(
+    require('./level')(),
+    require('blok-parse'),
+    digest,
+    function(error, value) {
+      console.log(require('blok-stringify')(value)) }) }
--- put.js
+++ put.js
@@ -1,10 +1,13 @@
 module.exports = function() {
-  var put = require('blok-put')
-    .bind(this, require('./level')(), require('sha256'))
   var input = ''
   process.stdin
     .on('data', function(buffer) {
       input += buffer.toString() })
     .on('end', function() {
-      put(require('blok-parse')(input), function(error, digest) {
-        console.log(digest) }) }) }
+      require('blok-put')(
+        require('./level')(),
+        require('blok-stringify'),
+        require('sha256'),
+        require('blok-parse')(input),
+        function(error, digest) {
+          console.log(digest) }) }) }
